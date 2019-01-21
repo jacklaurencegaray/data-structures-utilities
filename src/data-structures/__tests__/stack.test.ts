b00b5b30fc82246150fd674ce0c762966b8a66d3
jacklaurencegaray <lawrencetecho@gmail.com>
2019-01-21 20:19:19 +0800

    Working Stack test

--- src/data-structures/__tests__/stack.test.ts
+++ src/data-structures/__tests__/stack.test.ts
@@ -1,16 +1,25 @@
-import Stack from "../stack"
+import Stack from "../stack";
 
 test("stack push and pop works", () => {
-  let stack = new Stack<Number>()
-  stack.push(1)
-  stack.push(2)
+  let stack = new Stack<Number>();
+  stack.push(1);
+  stack.push(2);
 
-  expect(stack.pop()).toBe(2)
-  expect(stack.pop()).toBe(1)
-})
+  expect(stack.pop()).toBe(2);
+  expect(stack.pop()).toBe(1);
+});
 
 test("stack pop works even when empty", () => {
-  let stack = new Stack<Number>()
+  let stack = new Stack<Number>();
 
-  expect(stack.pop()).toBe(undefined)
-})
+  expect(stack.pop()).toBe(undefined);
+});
+
+test("stack can be initialized with predefined elements", () => {
+  let stack = new Stack<Number>([1, 2, 3, 4]);
+
+  stack.pop();
+  stack.pop();
+
+  expect(stack.pop()).toBe(2);
+});
