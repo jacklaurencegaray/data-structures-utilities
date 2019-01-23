551a6197ad3eeb245f94c0780298fd237925368b
jacklaurencegaray <lawrencetecho@gmail.com>
2019-01-23 18:18:16 +0800

    Initial binary tree test

---
+++ src/data-structures/__tests__/binary-tree.test.ts
@@ -0,0 +1,8 @@
+test("dfs preview is working", () => {});
+
+describe("binary tree operations should work", () => {
+  test("insert should work", () => {});
+  test("delete should work", () => {});
+  test("update should work", () => {});
+  test("search should work", () => {});
+});
