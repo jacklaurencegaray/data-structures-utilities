474f51db123ef4327651e096601a96a4a4db3129
jacklaurencegaray <lawrencetecho@gmail.com>
2019-02-15 12:15:11 +0800

    Renamed for consistent file naming

---
+++ src/algorithms/quick-sort.ts
@@ -0,0 +1,37 @@
+export default function sort(arr) {
+  const newArr = arr.slice();
+  quickSort(newArr, 0, newArr.length - 1);
+  return newArr;
+}
+
+function quickSort(arr, low, high) {
+  const partitionIndex = partition(arr, low, high);
+
+  if (low < high) {
+    quickSort(arr, low, partitionIndex - 1);
+    quickSort(arr, partitionIndex + 1, high);
+  }
+}
+
+function partition(arr, low, high) {
+  const pivotValue = arr[high];
+  let left = -1;
+  let right = 0;
+
+  while (right < high) {
+    if (pivotValue > arr[right]) {
+      left++;
+      let temp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = temp;
+    }
+    right++;
+  }
+
+  left++;
+  let temp = arr[left];
+  arr[left] = arr[right];
+  arr[right] = temp;
+
+  return left;
+}
